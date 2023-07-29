120e40b604d1f65c53833d2c9adc5a33bdc769ca
kavan7 <abeyratnekavan@gmail.com>
2023-07-29 18:03:56 -0400

    new change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,12 +56,12 @@ const Hero = () => {
         }}>
          
         
-        <a href="experience" className="font-black text-white mt-2">
+        <a href="#experience" className="font-black text-white mt-2">
         <svg data-accordion-icon class=" sm:block hidden w-16 h-10 rotate-180 shrink-0 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
       </svg>
         </a>
-                <a href="experience" className={`mt-14 ${styles.heroHeadText}`}>
+                <a href="#experience" className={`mt-14 ${styles.heroHeadText}`}>
         <svg data-accordion-icon class="sm:block hidden  w-16 h-10 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
       </svg>
